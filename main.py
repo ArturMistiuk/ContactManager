c94fa46e91ee8213afd4961727b73af11c6f11f1
Artur Mistiuk <mistiuk.artur@gmail.com>
2023-06-28 22:28:50 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI
 
-from src.routes import contacts
+from src.routes import contacts, auth
 
 app = FastAPI()
 
 app.include_router(contacts.router, prefix='/api')
+app.include_router(auth.router, prefix='/api')
 
 
 @app.get('/')

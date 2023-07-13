2f99dbc011246ad20bccdec061e10412f68516c9
Artur Mistiuk <mistiuk.artur@gmail.com>
2023-07-13 16:41:49 +0200

    Update auth.py

--- src/routes/auth.py
+++ src/routes/auth.py
@@ -17,17 +17,17 @@ security = HTTPBearer()
 @router.post("/signup", response_model=UserResponse, status_code=status.HTTP_201_CREATED)
 async def signup(body: UserModel,  background_tasks: BackgroundTasks, request: Request, db: Session = Depends(get_db)):
     """
-    The signup function creates a new user in the database.
+The signup function creates a new user in the database.
     It takes a UserModel object as input, which is validated by pydantic.
     If the email address already exists in the database, an HTTP 409 error is raised.
     The password field of the UserModel object is hashed using Argon2 and stored in that form.
     A new user record with this information is created and returned to the client.
 
-    :param body: UserModel: Get the data from the request body
-    :param background_tasks: BackgroundTasks: Add a task to the background tasks queue
-    :param request: Request: Get the base url of the server
-    :param db: Session: Get the database session
-    :return: A dictionary with two keys: user and detail
+:param body: UserModel: Get the data from the request body
+:param background_tasks: BackgroundTasks: Add a task to the background tasks queue
+:param request: Request: Get the base url of the server
+:param db: Session: Get the database session
+:return: A dictionary with two keys: user and detail
     """
     exist_user = await repository_users.get_user_by_email(body.email, db)
     if exist_user:
@@ -41,13 +41,13 @@ async def signup(body: UserModel,  background_tasks: BackgroundTasks, request: R
 @router.post("/login", response_model=TokenModel)
 async def login(body: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
     """
-    The login function is used to authenticate a user.
+The login function is used to authenticate a user.
     It takes the username and password from the request body,
     verifies that they are correct, and returns an access token.
 
-    :param body: OAuth2PasswordRequestForm: Get the username and password from the request body
-    :param db: Session: Get a database session
-    :return: A dictionary with the access token and refresh token
+:param body: OAuth2PasswordRequestForm: Get the username and password from the request body
+:param db: Session: Get a database session
+:return: A dictionary with the access token and refresh token
     """
     user = await repository_users.get_user_by_email(body.username, db)
     if user is None:
@@ -66,14 +66,13 @@ async def login(body: OAuth2PasswordRequestForm = Depends(), db: Session = Depen
 @router.get('/refresh_token', response_model=TokenModel)
 async def refresh_token(credentials: HTTPAuthorizationCredentials = Security(security), db: Session = Depends(get_db)):
     """
-    The refresh_token function is used to refresh the access token.
+The refresh_token function is used to refresh the access token.
     The function takes in a refresh token and returns an access_token, a new refresh_token, and the type of token.
     If the user's current refresh_token does not match what was passed into this function then it will return an error.
 
-    :param credentials: HTTPAuthorizationCredentials: Get the token from the request header
-    :param db: Session: Get the database session
-    :return: A dictionary with the access_token, refresh_token and token type
-
+:param credentials: HTTPAuthorizationCredentials: Get the token from the request header
+:param db: Session: Get the database session
+:return: A dictionary with the access_token, refresh_token and token type
     """
     token = credentials.credentials
     email = await auth_service.decode_refresh_token(token)
@@ -91,16 +90,16 @@ async def refresh_token(credentials: HTTPAuthorizationCredentials = Security(sec
 @router.get('/confirmed_email/{token}')
 async def confirmed_email(token: str, db: Session = Depends(get_db)):
     """
-    The confirmed_email function is used to confirm a user's email address.
+The confirmed_email function is used to confirm a user's email address.
     It takes the token from the URL and uses it to get the user's email address.
     The function then checks if there is a user with that email in our database, and if not, returns an error message.
     If there is a user with that email in our database, we check whether their account has already been confirmed or not.
         If it has been confirmed already, we return another error message saying so; otherwise we call repository_users'
         confirmed_email function which sets the 'confirmed' field of that particular User object
 
-    :param token: str: Get the token from the request
-    :param db: Session: Get the database session
-    :return: A message that the email is already confirmed or a message that the email has been confirmed
+:param token: str: Get the token from the request
+:param db: Session: Get the database session
+:return: A message that the email is already confirmed or a message that the email has been confirmed
 
     """
     email = await auth_service.get_email_from_token(token)
@@ -117,17 +116,17 @@ async def confirmed_email(token: str, db: Session = Depends(get_db)):
 async def request_email(body: RequestEmail, background_tasks: BackgroundTasks, request: Request,
                         db: Session = Depends(get_db)):
     """
-    The request_email function is used to send an email to the user with a link that they can click on
+The request_email function is used to send an email to the user with a link that they can click on
     to confirm their email address. The function takes in a RequestEmail object, which contains the
     email of the user who wants to confirm their account. It then checks if there is already a confirmed
     user with that email address, and if so returns an error message saying as much. If not, it sends
     an email containing a confirmation link.
 
-    :param body: RequestEmail: Get the email from the request body
-    :param background_tasks: BackgroundTasks: Add a task to the background tasks queue
-    :param request: Request: Get the base_url of the application
-    :param db: Session: Get the database session
-    :return: A message to the user, informing them that they should check their email for a
+:param body: RequestEmail: Get the email from the request body
+:param background_tasks: BackgroundTasks: Add a task to the background tasks queue
+:param request: Request: Get the base_url of the application
+:param db: Session: Get the database session
+:return: A message to the user, informing them that they should check their email for a
 
     """
     user = await repository_users.get_user_by_email(body.email, db)

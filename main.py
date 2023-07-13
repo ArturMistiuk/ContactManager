292c72dc42f298d1f6277359ae9c5c454d9b901b
Artur Mistiuk <mistiuk.artur@gmail.com>
2023-07-13 03:17:37 +0200

    Update main.py

--- main.py
+++ main.py
@@ -27,6 +27,12 @@ app.add_middleware(
 
 @app.on_event("startup")
 async def startup():
+    """
+    The startup function is called when the application starts up.
+    It's a good place to initialize things that are used by the app, such as databases or caches.
+
+    :return: FastAPILimiter(RedisConnection)
+    """
     r = await redis.Redis(host=settings.redis_host, port=settings.redis_port, db=0, encoding="utf-8",
                           decode_responses=True)
     await FastAPILimiter.init(r)
